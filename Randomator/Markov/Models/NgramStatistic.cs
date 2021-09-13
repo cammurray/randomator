f334c2d14988999daa1107fe022bc4823851078e
camurray <camurray@microsoft.com>
2021-09-14 08:28:43 +1000

    Markov implementation

---
+++ Randomator/Markov/Models/NgramStatistic.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * 
+ * Code taken from https://github.com/chriscore/MarkovSharp/
+ * 
+ * Base code by Chris Core. MIT License, see LICENSE file.
+ * 
+ * Modifications have been made for Randomator.
+ *
+ */
+ 
+namespace MarkovSharp.Models
+{
+    public class NgramStatistic<TNgram>
+    {
+        public TNgram Value { get; set; }
+        public double Count { get; set; }
+        public double Probability { get; set; }
+    }
+}
